e498ec5af71392b76eb997f2d95f8b6a74edbb4d
Frederico <fredeschpe@gmail.com>
2023-02-15 21:10:30 -0300

    First commit

---
+++ PaginacaoView/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PaginacaoView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ PaginacaoView/UCDayGrid.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PaginacaoView
+{
+    public partial class UcDayGrid : UserControl 
+    {
+        public ContentControl[] Horarios { get; private set; }
+
+        public static readonly DependencyProperty DayNameProperty = DependencyProperty.Register(
+            nameof(DayName), typeof(string), typeof(UcDayGrid), new PropertyMetadata(default(string)));
+        public string DayName
+        {
+            get => (string)GetValue(DayNameProperty);
+            set => SetValue(DayNameProperty, value);
+        }
+
+        public UcDayGrid()
+        {
+            InitializeComponent();
+            Horarios = new [] { H0, H1, H2, H3, H4 };
+        }
+    }
+}
